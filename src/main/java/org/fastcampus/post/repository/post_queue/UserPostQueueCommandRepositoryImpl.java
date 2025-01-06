4ebb07d496b4ce8905dd2607a23dff0e8d010867
suby_kwak <rhkdbtj@gmail.com>
2025-01-06 16:57:16 +0900

    [Refactor] Feed 인수테스트 error 해결

--- src/main/java/org/fastcampus/post/repository/post_queue/UserPostQueueCommandRepositoryImpl.java
+++ src/main/java/org/fastcampus/post/repository/post_queue/UserPostQueueCommandRepositoryImpl.java
@@ -4,7 +4,9 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.fastcampus.post.repository.entity.post.PostEntity;
+import org.fastcampus.post.repository.entity.post.UserPostQueueEntity;
 import org.fastcampus.post.repository.jpa.JpaPostRepository;
+import org.fastcampus.post.repository.jpa.JpaUserPostQueueRepository;
 import org.fastcampus.user.repository.entity.UserEntity;
 import org.fastcampus.user.repository.jpa.JpaUserRelationRepository;
 import org.springframework.stereotype.Repository;
@@ -15,8 +17,8 @@ public class UserPostQueueCommandRepositoryImpl implements UserPostQueueCommandR
 
     private final JpaPostRepository jpaPostRepository;
     private final JpaUserRelationRepository jpaUserRelationRepository;
-    //    private final JpaUserPostQueueRepository jpaUserPostQueueRepository;
-    private final UserQueueRedisRepository redisRepository;
+    private final JpaUserPostQueueRepository jpaUserPostQueueRepository;
+//    private final UserQueueRedisRepository redisRepository;
 
     @Override
     @Transactional
@@ -24,12 +26,12 @@ public class UserPostQueueCommandRepositoryImpl implements UserPostQueueCommandR
         UserEntity userEntity = postEntity.getAuthor();
         List<Long> followersIds = jpaUserRelationRepository.findFollowers(userEntity.getId());
 
-//        List<UserPostQueueEntity> userPostQueueEntityList = followersIds.stream()
-//            .map(userId -> new UserPostQueueEntity(userId, postEntity.getId(), userEntity.getId()))
-//            .toList();
-//
-//        jpaUserPostQueueRepository.saveAll(userPostQueueEntityList);
-        redisRepository.publishPostToFollowingUserList(postEntity, followersIds);
+        List<UserPostQueueEntity> userPostQueueEntityList = followersIds.stream()
+            .map(userId -> new UserPostQueueEntity(userId, postEntity.getId(), userEntity.getId()))
+            .toList();
+
+        jpaUserPostQueueRepository.saveAll(userPostQueueEntityList);
+//        redisRepository.publishPostToFollowingUserList(postEntity, followersIds);
     }
 
     @Override
@@ -37,18 +39,18 @@ public class UserPostQueueCommandRepositoryImpl implements UserPostQueueCommandR
     public void saveFollowPost(Long userId, Long targetId) {
         List<PostEntity> postIdList = jpaPostRepository.findAllPostIdsByAuthorId(targetId);
 
-//        List<UserPostQueueEntity> userPostQueueEntityList = postIdList.stream()
-//            .map(post -> new UserPostQueueEntity(userId, post.getId(), targetId))
-//            .toList();
-//
-//        jpaUserPostQueueRepository.saveAll(userPostQueueEntityList);
-        redisRepository.publishPostListToFollowerUser(postIdList, userId);
+        List<UserPostQueueEntity> userPostQueueEntityList = postIdList.stream()
+            .map(post -> new UserPostQueueEntity(userId, post.getId(), targetId))
+            .toList();
+
+        jpaUserPostQueueRepository.saveAll(userPostQueueEntityList);
+//        redisRepository.publishPostListToFollowerUser(postIdList, userId);
     }
 
     @Override
     @Transactional
     public void deleteUnfollowPost(Long userId, Long targetId) {
-//        jpaUserPostQueueRepository.deleteAllByUserIdAndAuthorId(userId, targetId);
-        redisRepository.deleteFeed(userId, targetId);
+        jpaUserPostQueueRepository.deleteAllByUserIdAndAuthorId(userId, targetId);
+//        redisRepository.deleteFeed(userId, targetId);
     }
 }
